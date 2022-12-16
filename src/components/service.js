6501c38ea98351ccc4eca675a51e3489adb50827
Vania_Dmitriienko <dmitrienko2706@gmail.com>
2022-12-16 20:53:16 +0200

    add authOperation

--- src/components/service.js
+++ src/components/service.js
@@ -12,3 +12,16 @@ export const notify = name => {
     theme: 'light',
   });
 };
+
+export const notifyAuthReject = () => {
+  toast.error(`Enter valid login or password`, {
+    position: 'top-right',
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+};
